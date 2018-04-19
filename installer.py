3d5f3df5b825f06fa909541f28b8d3f48c6697f4
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-19 19:39:20 +0900

    Fix ver python3

--- installer.py
+++ installer.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 import csv
 
-with open('/Users/k00121/Desktop/【Buzz】社員情報一覧/社員名簿-表1.csv', 'r') as f:
-    reader = csv.reader(f)
-    header = next(reader)  # ヘッダーを読み飛ばしたい時
-
-    for row in reader:
-        print row
+with open('/Users/k00121/Desktop/【Buzz】社員情報一覧/社員名簿-表1.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    #header = next(reader)  # ヘッダーを読み飛ばしたい時
+    for row in spamreader:
+        print(', '.join(row))
