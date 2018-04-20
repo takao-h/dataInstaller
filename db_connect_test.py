a418fd26e21103f5ea839628c7bf69f1b92000f9
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-20 10:18:47 +0900

    add new test

---
+++ db_connect_test.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from urllib.parse import urlparse
+import mysql.connector
+
+url = urlparse('mysql://user:pass@localhost:3306/test_for_py')
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'localhost',
+  'database': 'test_for_py',
+}
+cnx = mysql.connector.connect(**config)
+cur = cnx.cursor(buffered=True)
+cur.execute("SHOW STATUS LIKE 'Uptime'")
+print(cur.fetchone())
+# => ('Uptime', '22073875')
+
+stm = "create table user"
+cur.execute(stm)
+
+# => True
+cnx.close()
